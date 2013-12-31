950cf994421eb6a43814caebeb6fe6b82d4e0130
Feonik <nikita_k@ur.net>
2013-12-31 04:07:11 +0100

    Initial commit

---
+++ src/com/BibleQuote/BibleQuoteJavaFX/BibleQuoteApp.java
@@ -0,0 +1,66 @@
+package com.BibleQuote.BibleQuoteJavaFX;
+
+import com.BibleQuote.bqtj.jvm.CoreContextJvm;
+import com.BibleQuote.bqtj.CoreContext;
+import com.BibleQuote.bqtj.utils.LogTxt;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nikita K.
+ * Date: 10.11.13
+ * Time: 14:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class BibleQuoteApp extends Application {
+
+	private static final String TAG = "BibleQuoteApp";
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public BibleQuoteApp() {
+		super();
+	}
+
+	// NOTE: This method is NOT called on the JavaFX Application Thread.
+	// An application must not construct a Scene or a Stage in this method.
+	// An application may construct other JavaFX objects in this method.
+	@Override
+	public void init() {
+
+	}
+
+	// NOTE: This method is called on the JavaFX Application Thread.
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		getCoreContext().Init();
+
+		primaryStage.setTitle("BibleQuoteJavaFX");
+		Pane myPane = (Pane) FXMLLoader.load(getClass().getResource("ui/res/layout/SplashActivity.fxml"));
+		Scene myScene = new Scene(myPane, 400, 500);
+		primaryStage.setScene(myScene);
+		primaryStage.show();
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	public CoreContext getCoreContext() {
+		return CoreContextJvm.getCoreContextJvm();
+	}
+
+}
---
+++ src/com/BibleQuote/BibleQuoteJavaFX/Controller.java
@@ -0,0 +1,27 @@
+package com.BibleQuote.BibleQuoteJavaFX;
+
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+
+//	@FXML
+//	private Pane leftPart;
+//	@FXML
+//	private WebView webView;
+
+
+	@Override
+	public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+		//SplitPane.setResizableWithParent(leftPart, Boolean.FALSE);
+	}
+
+
+//	@FXML
+//	protected void handleSubmitButtonAction(MouseEvent event) {
+//		final WebEngine eng = webView.getEngine();
+//		eng.load("http://jesuschrist.ru/bible/?b=19&c=118");
+//	}
+
+}
+
+
